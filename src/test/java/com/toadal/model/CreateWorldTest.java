56748f3282e9bc77c784335ae6f199f2bf8df203
bobpaige <bobpaige@github.com>
2015-12-27 19:55:53 -0500

    Update CreateWorldTest

--- src/test/java/com/toadal/model/CreateWorldTest.java
+++ src/test/java/com/toadal/model/CreateWorldTest.java
@@ -1,5 +1,16 @@
 package com.toadal.model;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CreateWorldTest {
-    
+    @Test
+    public void testCreateWorld(){
+        World w = new World();
+        
+        assertNotNull("World was null", w.getTerritories());
+//        Assert.assertNotEquals("No territories added",  0, w.getTerritories().size());
+    }
 }
